ae9975b58b114719eb996646d284a301d2918cc7
Fumihiko Kinoshita <fkinos@gmail.com>
2022-06-12 10:34:56 +0900

    Input from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::io::Read;
+use std::fs::File;
 use clap::Parser;
+
+const MFA_SERIAL_FILE: &str = ".mfaserial";
 
 /// Automation for temporary MFA credentials
 #[derive(Parser, Debug)]
@@ -11,6 +15,16 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    let mfa_token = args.token;
 
-    println!("{}", args.token);
+    let mut f = File::open(MFA_SERIAL_FILE)
+        .expect("MFA serial file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    let mfa_serial = contents.trim();
+
+    println!("{}", mfa_token);
+    println!("{}", mfa_serial);
+
 }
